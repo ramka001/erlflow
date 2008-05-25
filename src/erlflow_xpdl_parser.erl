60a37808232b8314d791a780488560ed0194270c
josemanuelp2 <josemanuelp2@80e387fa-224d-0410-bf74-f345a8662983>
2008-05-25 21:08:10 +0000

    json and couchdb support

--- src/erlflow_xpdl_parser.erl
+++ src/erlflow_xpdl_parser.erl
@@ -3,58 +3,13 @@
 
 -export([process/1,net_sender_xpdl/2]).
 
-get_id_attr(Node) ->
-    #xmlElement{attributes=Attribs} = Node,
-    [ #xmlAttribute{name=id, value=ID}] = Attribs,
-    list_to_atom(ID). 
-
-get_id_attr_arc(Node) ->
-    #xmlElement{attributes=Attribs} = Node,
-    [ #xmlAttribute{name=target, value=Target},  #xmlAttribute{name=source, value=Source},  #xmlAttribute{name=id, value=ID}] = Attribs,
-    {list_to_atom(ID), list_to_atom(Target), list_to_atom(Source)}. 
-
-extract_name(R, L) when is_record(R, xmlElement) ->
-    lists:foldl(fun extract_name/2, L, R#xmlElement.content);
-extract_name(#xmlText{parents=[{text,_},{name,_},{_,_},{net,_},{pnml,_}], value=V}, _) ->
-    V;
-extract_name(_, L) ->
-    L.
-
-create_object([Node|Rest]) ->
-    case Node#xmlElement.name of
-        place ->
-            ID = get_id_attr(Node),
-            Name = extract_name(Node, []),
-            register(ID, spawn(erlflow_net, place, [ID,Name,[],[],[]])),
-            netsuper ! {add_place, ID},
-            io:format("new place ~s:~p ~n", [ID, Name]),
-            create_object(Rest);
-        transition ->
-            ID = get_id_attr(Node),
-            Name = extract_name(Node, []),
-            register(ID, spawn(erlflow_net, transition, [ID,Name,[],[]])),
-            netsuper ! {add_transition, ID},
-            io:format("new transition ~s:~p ~n", [ID, Name]),
-            create_object(Rest);
-        arc ->
-            ID = get_id_attr_arc(Node),
-            {_, Target, Source} = ID,
-            Source ! {output, Target},
-            Target ! {input, Source},
-            io:format("new arc ~w~n", [ID]),
-            create_object(Rest);
-        _else ->
-            create_object(Rest)
-    end;
-create_object([]) ->
-    done.
-
 
 process(FName) -> 
     {R,_} = xmerl_scan:file(FName),
     register(net_sender_xpdl, spawn(erlflow_xpdl_parser, net_sender_xpdl, [[],[]])),
 	io:format("reading XPDL file~n"),
-    extract(R, []).
+    extract(R, [])
+.
 
 net_sender_xpdl(Id, Name) ->
 	receive
