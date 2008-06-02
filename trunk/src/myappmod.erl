8a4b44fd8e9c9029372d156817e55922ba872ed8
josemanuelp2 <josemanuelp2@80e387fa-224d-0410-bf74-f345a8662983>
2008-06-02 04:19:45 +0000

    Incorporating Yahoo User Interface... json in appmods

--- trunk/src/myappmod.erl
+++ trunk/src/myappmod.erl
@@ -11,25 +11,12 @@ box(Str) ->
 out(A) ->	
     netsuper ! {get_status, self()},
     receive Networks -> Net = Networks end,
-    %JsonMap = lists:map(fun(X) -> if is_atom(X) -> atom_to_list(X); true -> X end end, Net),
-    JsonVar = prepare_json([Net]),
-    %JVar =  rfc4627:encode([sksksi,"sksks"]),
-    JVar = ktuo_json:encode([{string, "wwwdddd"},{string, "ksksks"}]),
-    %JVar = json:encode(JsonVar),
-    {ehtml,
-     [{p,[],
-       box(io_lib:format("A#arg.appmoddata1 = ~p~n"
-                         "A#arg.appmod_prepath = ~p~n"
-                         "A#arg.querydata = ~p~n" 
-                         "Json = ~p~n", 
-                         [A#arg.appmoddata,
-                          A#arg.appmod_prepath,
-                          A#arg.querydata,
-			  JsonVar
-			 ]))}]}.
-
-prepare_json2({Key , Value}) ->
-	{Key, {string, Value}}.
+    {content, "text/plain",  ktuo_json:encode(prepare_json([Net]))}.
+             
+prepare_json({Key, Value}) ->
+  io:format("thread was here ~n"),
+  {Key, {string, Value}};
 prepare_json([Head | Tail]) ->
-	prepare_json2(Head),
-	prepare_json(Tail).
+  lists:merge([prepare_json(Head)],prepare_json(Tail));
+prepare_json([]) -> [] .
+
